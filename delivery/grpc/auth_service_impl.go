593b3f2bc3f748a419f55f86baebd70d27b23e57
alramdein <ramdanialif26@gmail.com>
2022-12-23 10:29:16 +0700

    feature: add rra enum

--- delivery/grpc/auth_service_impl.go
+++ delivery/grpc/auth_service_impl.go
@@ -8,7 +8,12 @@ import (
 )
 
 func (s *Service) HasAccess(ctx context.Context, req *pb.HassAccessRequest) (res *pb.HasAccessResponse, err error) {
-	hasAccess := s.authUsecase.HasAccess(ctx, req.GetRoleId(), model.Resource(req.GetResource()), model.Action(req.GetAction()))
+	hasAccess := s.authUsecase.HasAccess(
+		ctx,
+		req.GetRoleId(),
+		model.GetResoruce(req.GetResource()),
+		model.GetAction(req.GetAction()),
+	)
 	if hasAccess != true {
 		res = &pb.HasAccessResponse{HasAccess: false}
 		return
--- model/auth.go
+++ model/auth.go
@@ -1,6 +1,10 @@
 package model
 
-import "context"
+import (
+	"context"
+
+	"github.com/alramdein/auth-service/pb"
+)
 
 type Role string
 type Resource string
@@ -27,12 +31,39 @@ var RoleMap = map[int64]Role{
 	2: RoleMember,
 }
 
+var ResourceMap = map[pb.Resource]Resource{
+	pb.Resource_ResourceUser: UserResource,
+}
+
+var ActionMap = map[pb.Action]Action{
+	pb.Action_ActionCreate: Create,
+	pb.Action_ActionUpdate: Update,
+	pb.Action_ActionDelete: Delete,
+	pb.Action_ActionView:   View,
+}
+
 func GetRole(role_id int64) Role {
 	r, err := RoleMap[role_id]
 	if err != true {
 		return RoleMember
 	}
 	return r
+}
+
+func GetResoruce(resource pb.Resource) Resource {
+	r, err := ResourceMap[resource]
+	if err != true {
+		return UserResource
+	}
+	return r
+}
+
+func GetAction(action pb.Action) Action {
+	a, err := ActionMap[action]
+	if err != true {
+		return View
+	}
+	return a
 }
 
 type AuthUsecase interface {
